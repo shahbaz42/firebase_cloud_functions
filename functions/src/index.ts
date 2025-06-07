4aa548884040055fcae8ce84bd2fd3f8d29d3559
Shahbaz Ali <shahbaz_ali@outlook.in>
2025-06-08 02:37:41 +0530

    add triggers, store user data in firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,27 @@
 import { onCall, onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { auth } from "firebase-functions/v1";
+import * as admin from "firebase-admin";
+admin.initializeApp()
+
+
+// auth trigger (new user signup)
+exports.newUserSignup = auth.user().onCreate((user) => {
+    console.log("New user signed up:", user.email, user.uid);
+    // to return value or promise
+    return admin.firestore().collection("users").doc(user.uid).set({
+        email: user.email,
+        upvotedOn: [],
+    });
+});
+
+// auth trigger (new user deleted)
+exports.userDeleted = auth.user().onDelete((user) => {
+    console.log("User Deleted:", user.email, user.uid);
+    // to return value or promise
+    const doc = admin.firestore().collection("users").doc(user.uid);
+    return doc.delete();
+});
 
 // http request 1
 exports.randomNumber = onRequest({ region: "asia-south1" }, (req, res) => {
