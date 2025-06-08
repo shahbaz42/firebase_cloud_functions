c61f6a34fb244ee58000dc58d3cfcf4950b93511
Shahbaz Ali <shahbaz_ali@outlook.in>
2025-06-08 17:13:00 +0530

    code refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -38,28 +38,6 @@ requestForm.addEventListener('submit', (e) => {
         });
 });
 
-// say hello function call
-const button = document.querySelector('.call');
-
-// button.addEventListener('click', () => {
-//     // get region-specific function reference
-//     const functions = firebase.app().functions('asia-south1');
-
-//     // connect to local emulator
-//     // functions.useEmulator('localhost', 5001);
-
-//     const sayHello = functions.httpsCallable('sayHello');
-//     sayHello({
-//         name: "Shahbaz Ali"
-//     }).then((result) => {
-//         console.log(result.data);
-//         alert(result.data);
-//     }).catch((error) => {
-//         console.error("Error:", error);
-//         alert("Error: " + error.message);
-//     });
-// });
-
 // notification
 const notification = document.querySelector('.notification');
 
